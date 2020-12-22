182fa98c3fdb2d4709ca618f7e23b12aebdbdac2
IanCode09 <ianlombu@protonmail.com>
2020-12-22 14:28:37 +0700

    Error handler & Create Redux

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import asyncHandler from 'express-async-handler'
 const router = express.Router()
 import Product from '../models/productModel.js'
 
@@ -8,5 +9,18 @@ router.get('/', async (req, res) => {
 
     res.json(products)
 })
+
+
+// @desc    Fetch Product by Id
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        res.json(product)
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+}))
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './db/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
 
@@ -10,11 +11,16 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Backend Running ...')
 })
 
 app.use('/server/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 8080
 
