ad08eab1726a325c26363393ca98bd4bb2e6f7aa
IanCode09 <ianlombu@protonmail.com>
2020-12-27 00:14:22 +0700

    User Registration & Password Hashing

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -43,4 +43,36 @@ router.get('/profile', signMiddleware, asyncHandler(async (req, res) => {
     }
 }))
 
+
+// @desc   User Register
+router.post('/register', asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExist = await User.findOne({ email })
+
+    if(userExist) {
+        res.status(400)
+        throw new Error('User already')
+    }
+
+    const user = await User.create({
+        name,
+        email, 
+        password
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: signToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user info')
+    }
+}))
+
 export default router
